9b962f8d717bc9548282f34f95853b3bf5955256
Pursoth <pursothpersonal@gmail.com>
2024-02-24 18:20:20 +0530

    splash animation finished

--- lib/pages/splash_screen/splash.dart
+++ lib/pages/splash_screen/splash.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:pet_care/common/utils/app_const.dart';
 import 'package:pet_care/common/utils/colors.dart';
+import 'package:pet_care/common/utils/constants.dart';
 import 'package:pet_care/common/utils/image_string.dart';
 
 class Splash extends StatefulWidget {
@@ -13,31 +14,45 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> with TickerProviderStateMixin {
-  final cardSize = 180.0;
+  final cardSize = 150.0;
 
-  late final cardOffsetAnimationController = AnimationController(
-    vsync: this,
-    duration: const Duration(milliseconds: 1000),
-  );
+  /****************** TWEEN ************************/
 
   late final holeSizeTween = Tween<double>(
     begin: 0,
     end: 1.5 * cardSize,
   );
 
+  late final cardRotationTween = Tween<double>(
+    begin: 0.95,
+    end: 0,
+  );
+
+  late final cardOffsetTween = Tween<double>(
+    begin: 12,
+    end: 0,
+  ).chain(CurveTween(curve: Curves.easeInOutBack));
+
+  /****************** CONTROLLER ************************/
+
   late final holeAnimationController = AnimationController(
     vsync: this,
-    duration: const Duration(milliseconds: 500),
+    duration: const Duration(milliseconds: 600),
+  );
+
+  late final cardOffsetAnimationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 1500),
   );
 
   double get holeSize => holeSizeTween.evaluate(holeAnimationController);
+  double get cardRotation => cardRotationTween.evaluate(cardOffsetAnimationController);
+  double get cardOffset => cardOffsetTween.evaluate(cardOffsetAnimationController);
 
   @override
   void initState() {
     initAnimation();
-    holeAnimationController.addListener(() => setState(() {
-          // if (holeAnimationController.isCompleted) Future.delayed(const Duration(milliseconds: 400), () => holeAnimationController.reverse());
-        }));
+    holeAnimationController.addListener(() => setState(() {}));
     cardOffsetAnimationController.addListener(() => setState(() {}));
     super.initState();
   }
@@ -51,6 +66,7 @@ class _SplashState extends State<Splash> with TickerProviderStateMixin {
   Future<void> initAnimation() async {
     holeAnimationController.forward();
     await cardOffsetAnimationController.forward();
+    Future.delayed(const Duration(milliseconds: 300), () => holeAnimationController.reverse());
   }
 
   @override
@@ -61,6 +77,7 @@ class _SplashState extends State<Splash> with TickerProviderStateMixin {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Spacer(),
             Spacer(),
             Text.rich(
               TextSpan(
@@ -86,37 +103,43 @@ class _SplashState extends State<Splash> with TickerProviderStateMixin {
               ),
             ),
             Spacer(),
-            Spacer(),
             Container(
-              height: cardSize * 1.25,
+              height: cardSize * 1.45,
               child: Center(
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    SizedBox(
-                      width: holeSize,
-                      child: Image.asset(
-                        ImagePath.hole,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Transform.translate(
-                      offset: Offset(0, -50),
-                      child: Transform.rotate(
-                        angle: 0,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Image.asset(
-                            ImagePath.logo,
-                            fit: BoxFit.cover,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ClipPath(
+                    clipper: BlackHoleClipper(),
+                    child: Stack(
+                      alignment: Alignment.topCenter,
+                      children: [
+                        Positioned(
+                          bottom: 0,
+                          child: SizedBox(
+                            width: holeSize,
+                            child: Image.asset(
+                              ImagePath.hole,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
+                        Transform.translate(
+                          offset: Offset(0, cardOffset * 15),
+                          child: Transform.rotate(
+                            angle: cardRotation,
+                            child: Image.asset(
+                              ImagePath.logo,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
+            Spacer(),
             Spacer(),
             ElevatedButton(
               onPressed: () {
@@ -133,4 +156,30 @@ class _SplashState extends State<Splash> with TickerProviderStateMixin {
       ),
     );
   }
+}
+
+class BlackHoleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.moveTo(0, size.height / 2);
+    path.arcTo(
+      Rect.fromCenter(
+        center: Offset(size.width / 2, size.height / 2),
+        width: size.width,
+        height: size.height,
+      ),
+      0,
+      pi,
+      true,
+    );
+    // Using -1000 guarantees the card won't be clipped at the top, regardless of its height
+    path.lineTo(0, -1000);
+    path.lineTo(size.width, -1000);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(BlackHoleClipper oldClipper) => false;
 }
